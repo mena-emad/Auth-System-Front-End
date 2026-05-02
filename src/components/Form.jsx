51f0691295b07643f30f49862e37ed6fa2203275
Mena Emad <menaemad6543@gmail.com>
2026-05-02 20:14:09 +0300

    fixed 8 chars zodSchema login problem

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -28,7 +28,7 @@ export default function Form({ formType, ...rest }) {
 
   const zodSchemaLogin = z.object({
     email: z.string().min(1, { message: "Email is required" }).email({ message: "Invalid email address" }).trim(),
-    password: z.string().min(1, { message: "Password is required" }).min(8, { message: "Password must be at least 8 characters long" })
+    password: z.string().min(1, { message: "Password is required" }).min(6, { message: "Password must be at least 6 characters long" })
   });
 
   const { register, reset, handleSubmit, formState: { errors } } = useForm({
